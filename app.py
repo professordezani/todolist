a57326bcf4fcea7f19810c2ca600e85f142d1285
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2022-10-31 21:26:46 +0000

    Flask instalado

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Minha primeira aplicação web')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Web!'
+
+@app.route('/bye')
+def bye():
+    return 'Bye'
+
+app.run(debug=True)
