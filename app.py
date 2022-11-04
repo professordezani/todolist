bf2c8d8c047de3f9e65425d528528438bbcec21e
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2022-11-04 21:07:32 +0000

    render_template e bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Web!'
+    # templates/home.html
+    return render_template('home.html')
 
 @app.route('/bye')
 def bye():
