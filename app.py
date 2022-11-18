d03cb078cf56b5f82639c7560c5c34c64636e24b
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2022-11-18 21:23:32 +0000

    Adicionamos Forms

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+tasks = [
+    {'name': 'Estudar', 'finished': False},
+    {'name': 'Dormir', 'finished': True},
+    {'name': 'Comer', 'finished': True}
+]
 
 @app.route('/')
 def home():
     # templates/home.html
-    return render_template('home.html')
+    return render_template('home.html', tasks=tasks)
 
-@app.route('/bye')
-def bye():
-    return 'Bye'
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form['name']
+    task = {'name': name, 'finished': False}
+    tasks.append(task)
+    return render_template('home.html', tasks=tasks)
 
 app.run(debug=True)
